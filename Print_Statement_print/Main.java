898b7cc22f1635a30777abc6fb3bbe681c92a481
FahimFBA <fahimbinamin@gmail.com>
2022-09-19 13:32:00 +0600

    print concept simplified

---
+++ Print_Statement_print/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // Md. Fahim Bin Amin
+        // System.out.print("Md. Fahim Bin Amin");
+        // Md.
+        // Fahim
+        // Bin 
+        // Amin
+        // \n indicates a new line character
+        // System.out.print("Md.\n");
+        // System.out.print("Fahim\n");
+        // System.out.print("Bin\n");
+        // System.out.print("Amin");
+        System.out.print("Md.\nFahim\nBin\nAmin\n");
+    }
+}
