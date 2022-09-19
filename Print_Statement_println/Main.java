089a5dc39d7fcf9b7f1f6bee6afe5455062368f3
FahimFBA <fahimbinamin@gmail.com>
2022-09-19 13:21:12 +0600

    println concept

---
+++ Print_Statement_println/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Fahim");
+        System.out.println("Bin Amin");
+        // \n = new line statement
+        System.out.println("I am studying.");
+        System.out.println("I am studying at United International University");
+    }
+}
