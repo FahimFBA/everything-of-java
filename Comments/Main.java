f3ee441e16e341a7c7c6c0bfac967e3b31848762
FahimFBA <fahimbinamin@gmail.com>
2023-03-03 02:21:21 +0600

    comments

---
+++ Comments/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // printing a statement
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        // This is a single line comment
+
+        /*
+         * This is
+         * a
+         * multiline
+         * comment
+         */
+
+    }
+}
