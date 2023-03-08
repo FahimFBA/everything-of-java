43a1d1c76fb6a261612106418bae212e06869567
Md. Fahim Bin Amin <fahimbinamin@gmail.com>
2023-03-08 16:25:55 +0600

    Update Main.java

--- Print_Statement_print/Main.java
+++ Print_Statement_print/Main.java
@@ -1,16 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // Md. Fahim Bin Amin
-        // System.out.print("Md. Fahim Bin Amin");
-        // Md.
-        // Fahim
-        // Bin 
-        // Amin
-        // \n indicates a new line character
-        // System.out.print("Md.\n");
-        // System.out.print("Fahim\n");
-        // System.out.print("Bin\n");
-        // System.out.print("Amin");
-        System.out.print("Md.\nFahim\nBin\nAmin\n");
+//        System.out.print("Hello world!\n");
+//        System.out.println("Hello world for the second time!");
+//        System.out.println("Md. Fahim Bin Amin");
+//        System.out.print("Md. ");
+//        System.out.print("Fahim ");
+//        System.out.print("Bin ");
+//        System.out.print("Amin");
+//        System.out.print("Md.");
+//        System.out.print(" Fahim");
+//        System.out.print(" Bin");
+//        System.out.print(" Amin");
+//        System.out.print("Fahim   Amin");
+        System.out.print("Fahim\tAmin");
     }
 }
