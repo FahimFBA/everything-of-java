2fab3e8b5ee760f75d2240299849e6f9b0b440cb
FahimFBA <fahimbinamin@gmail.com>
2023-03-16 13:16:22 +0600

    var extended

---
+++ Variables_Extended_1/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+//        int myAge, myComputerAge; // variable declaration
+//        myAge = 25; // variable's value initialization
+//        myComputerAge = 20; // variable's value initialization
+//        int myAge;
+//        int myComputerAge;
+//        myAge = 25;
+//        myComputerAge = 20;
+        int myAge = 25;
+        int myComputerAge = 20;
+    }
+}
