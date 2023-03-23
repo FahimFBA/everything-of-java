14d28c3f427f6f2c2eb341239bac9e3a77b0f325
FahimFBA <fahimbinamin@gmail.com>
2023-03-24 02:05:39 +0600

    primitive data types

---
+++ Primitive_DataType/Main.java
@@ -0,0 +1,44 @@
+public class Main {
+    public static void main(String[] args) {
+        // byte = 8 bits
+        byte ByteVariable = 54;
+        System.out.println(ByteVariable);
+
+        // short = 16 bits
+        short ShortVariable = 78;
+        System.out.println(ShortVariable);
+
+        // int = 32 bits
+        int Age = 24;
+        System.out.println(Age);
+
+        int AnotherIntegerValue = 569_812;
+        System.out.println(AnotherIntegerValue);
+
+        // long = 64 bits
+        long AnotherVariable = 12345678910L;
+        System.out.println(AnotherVariable);
+
+        long NewData = 12_345_678_911_235L;
+        System.out.println(NewData);
+
+        // float = 32 bits
+        float FractionalValue = 59_856.987F;
+        System.out.println(FractionalValue);
+
+        // double = 64 bits
+        double DoubleValue = 48_987_156_894_56.694;
+        System.out.println(DoubleValue);
+
+        // char = 16 bits
+        char CharacterData = 'A';
+        System.out.println(CharacterData);
+
+        // boolean = 1 bit
+        boolean b = true;
+        System.out.println(b);
+
+        boolean v = false;
+        System.out.println(v);
+    }
+}
