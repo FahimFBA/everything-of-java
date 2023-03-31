40ec64a8658ddd49cde45bf29904ee355dc367fc
FahimFBA <fahimbinamin@gmail.com>
2023-03-31 21:13:12 +0600

    Create Main.java

---
+++ String/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Hello World");
+
+
+//        String sentence = new String("Hello World");
+//        System.out.println(sentence);
+
+        // The standard way
+//        String sentence = "Hello World";
+//        System.out.println(sentence);
+
+
+//        String Text = "We are humans";
+//        System.out.println(Text.length());
+
+//        String sentence1 = "Hello from " + "Fahim" + " !";
+//        System.out.println(sentence1);
+
+//        String newSentence = "I am Fahim";
+//        boolean flag  = newSentence.startsWith("a");
+//        System.out.println(flag);
+
+//        String newSentence = "I am Fahim";
+//        System.out.println(newSentence.startsWith("I am"));
+
+//        String newSentence = "I am Fahim";
+//        System.out.println(newSentence.endsWith("I am"));
+//        System.out.println(newSentence.endsWith("m"));
+//        System.out.println(newSentence.endsWith("Fahim"));
+//        System.out.println(newSentence.endsWith(" "));
+//        boolean flag = newSentence.endsWith("A");
+//        System.out.println(flag);
+
+//        String NewSentence = "We are learning Java. How cool is that!!!";
+//        System.out.println(NewSentence.indexOf('e'));
+//        System.out.println("Position for the first e in the string is: " + (NewSentence.indexOf('e') + 1));
+//        System.out.println(NewSentence.indexOf('e', 3));
+//        System.out.println(NewSentence.indexOf("cool"));
+//        System.out.println(NewSentence.indexOf("Fahim"));
+
+//        String MyNewString = "I am Fahim, and I am a Software Engineer @Microsoft";
+//        System.out.println(MyNewString);
+//        System.out.println(MyNewString.replace("@", "#"));
+//        System.out.println(MyNewString.replace("Microsoft", "Google"));
+//        System.out.println(MyNewString);
+//
+//        System.out.println(MyNewString.toUpperCase());
+//
+//        System.out.println(MyNewString.toLowerCase());
+//
+//        System.out.println(MyNewString);
+
+//        String Sentence = " I am Fahim  .   I am student.  ";
+//        System.out.println(Sentence);
+//        System.out.println(Sentence.trim());
+//
+//        String Sentence2 = "   I am Fahim. I am a student.     ";
+//        System.out.println(Sentence2.trim());
+//        System.out.println(Sentence2);
+
+//        String NewString = "I am Fahim. I study at United International University.";
+//        System.out.println(NewString);
+//        System.out.println(NewString.substring(14, 29));
+//        System.out.println(NewString.substring(2));
+//        System.out.println(NewString.charAt(5));
+
+        String Sentence = "   I am Fahim.";
+        String MyNewTrimmedString = Sentence.trim();
+        System.out.println(MyNewTrimmedString);
+        System.out.println(Sentence);
+    }
+}
