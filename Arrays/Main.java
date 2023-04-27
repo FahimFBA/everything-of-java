0aaed9df935e471c7b6d44230a2294325ac61927
FahimFBA <fahimbinamin@gmail.com>
2023-04-28 00:51:34 +0600

    1D Array

---
+++ Arrays/Main.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+        // int[] myArray = new int[3];
+        // myArray[0] = 50;
+        // myArray[1] = 80;
+        // System.out.println(Arrays.toString(myArray));
+        int[] myNewArray = { 5, 2, 9, 6 };
+        System.out.println(myNewArray.length);
+        System.out.println(Arrays.toString(myNewArray));
+    }
+}
