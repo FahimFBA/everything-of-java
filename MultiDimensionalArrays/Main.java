f6324bb71a526afd3aa7d04778cea8ab2061cf37
FahimFBA <fahimbinamin@gmail.com>
2023-05-15 01:30:03 +0600

    2D Array

---
+++ MultiDimensionalArrays/Main.java
@@ -0,0 +1,9 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main{
+    public static void main(String[] args) {
+        int[][] cubics = { {1, 2, 3, 5}, {4, 5, 6, 9}};
+        System.out.println(Arrays.deepToString(cubics));
+    }
+}
