9eefc9dba4107389df7a7b9538ea2c3b6f45c708
FahimFBA <fahimbinamin@gmail.com>
2023-05-16 00:26:55 +0600

    constant

---
+++ Constant/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    public static void main(String[] args) {
+        // Circle 1
+        // radius = 2
+        final float PI = 3.1416F;
+        int r1 = 2;
+        float area_circle1 = PI * r1 * r1;
+        System.out.println("Area for Circle 1: " + area_circle1);
+        // Circle 2
+        // radius = 5
+        int r2 = 5;
+        float area_circle2 = PI * r2 * r2;
+        System.out.println("Area for Circle 2: " + area_circle2);
+    }
+}
