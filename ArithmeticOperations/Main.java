4e6a812df5fdea02dadb8eac524be38dfc8374ed
FahimFBA <fahimbinamin@gmail.com>
2023-05-20 00:07:23 +0600

    arithmetic operations

---
+++ ArithmeticOperations/Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 80;
+        int addition = a + b;
+        System.out.println(addition);
+        int subtraction = a - b;
+        System.out.println(subtraction);
+        int multiplication = a * b;
+        System.out.println(multiplication);
+        double division = (double) b / (double) a;
+        System.out.println(division);
+        int modulus = a % b;
+        System.out.println(modulus);
+
+    }
+}
